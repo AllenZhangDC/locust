ba83a3a82d1429bc065608ec39c68bf5274b4a8b
Allen Zhang <allen.zhang@ganjing.dev>
2023-07-11 18:05:42 -0400

    workable Getone, Getgg and GetoneAndCallbacks

--- locustfile.py
+++ locustfile.py
@@ -12,76 +12,59 @@ jsonstr = '{"cid": "cid_BzanIxIY", "req_id": "req_OssjLsTY", "cnt_id": "cnt_Cmpg
 class Single_GetOne(HttpUser):
     host = "https://loadtest.dev.ganjing.world/v1/cdkapi"
     wait_time = between(5, 10)
+    probability = 0.6
     @task
     def get_onev2(self):
-        resp = self.client.get(url="/getonev2", params={"cid":random_text('cid_'), "cnt_id":random_text('cnt_'), "req_id":random_text('req_'), "lang":random_lang()}, name="Single_GetOne")
+            with self.client.get(url="/getonev2", params={"cid":random_text(), "cnt_id":random_text(), "req_id":random_text(), "lang":random_lang()}, catch_response=True, name="Single_GetOne") as resp:
+                code = resp.status_code
+                if code == 200:
+                    json_data = json.loads(resp.text)
+                    if json_data["data"]["is_404"] == True:
+                        resp.failure("is_404: True. NoAdsReason: " + json_data["data"]["no_ad_reason"])
+                    else:
+                        xml = base64.b64decode(json_data["data"]["xml"])
+                        self.Impression = ET.fromstring(xml).findall(".//Impression")[1].text
+            
+
+class Single_GetGG(HttpUser):
+    host = "https://loadtest.dev.ganjing.world/v1/cdkapi"
+    wait_time = between(5, 10)
+    @task
+    def get_ggv2(self):
+        json_body = generate_random_ggrequest_body()
+        resp = self.client.post(url="http://loadtest.dev.ganjing.world/v1/cdkapi/getggv2", json = json_body, name="Single_GetGG")
         if resp.status_code == 200:
-            json_data = json.loads(resp.text)
-            if json_data["data"]["is_404"] == True:
-                resp.failure("is_404: True. NoAdsReason: " + json_data["data"]["no_ad_reason"])
+            json_var = resp.json()
 
-            # xml = base64.b64decode(json_data["data"]["xml"])
-            # self.Impression = ET.fromstring(xml).findall(".//Impression")[1].text
-            # self.Skip = ET.fromstring(xml).findall(".//*[@event='skip']")[1].text
-            # self.Progress = ET.fromstring(xml).findall(".//*[@event='progress']")[1].text
-            # self.FirstQuartile = ET.fromstring(xml).findall(".//*[@event='firstQuartile']")[1].text
-            # self.Midpoint = ET.fromstring(xml).findall(".//*[@event='midpoint']")[1].text
-            # self.ThirdQuartile = ET.fromstring(xml).findall(".//*[@event='thirdQuartile']")[1].text
-            # self.Complete = ET.fromstring(xml).findall(".//*[@event='complete']")[1].text
-            # self.ClickTracking = ET.fromstring(xml).findall(".//ClickTracking")[0].text
-            #self.random_callback(probability=0.6)
-
-# class Single_GetGG(HttpUser):
-#     host = "https://loadtest.dev.ganjing.world/v1/cdkapi"
-#     wait_time = between(5, 10)
-#     @task
-#     def get_ggv2(self):
-#         json_body = generate_random_ggrequest_body()
-#         resp = requests.post(url="http://loadtest.dev.ganjing.world/v1/cdkapi/getggv2", json = json_body)
-#         if resp.status_code == 200:
-#             json_var = resp.json()
-
-# class GetOneWithCallback(HttpUser):
-#     host = "https://loadtest.dev.ganjing.world/v1/cdkapi"
-#     wait_time = between(5, 10)
-#     @task
-#     def get_onev2(self):
-#         with self.client.get(url="/getonev2", params={"cid":random_text(), "cnt_id":random_text(), "req_id":random_text(), "lang":random_lang()}, name="Video Ads Request",catch_response=True) as resp:
-#             code = resp.status_code
-#             if code == 200:
-#                 json_data = json.loads(resp.text)
-#                 if json_data["data"]["is_404"] == False:
-#                     xml = base64.b64decode(json_data["data"]["xml"])
-#                     self.Impression = ET.fromstring(xml).findall(".//Impression")[1].text
-#                     self.Skip = ET.fromstring(xml).findall(".//*[@event='skip']")[1].text
-#                     self.Progress = ET.fromstring(xml).findall(".//*[@event='progress']")[1].text
-#                     self.FirstQuartile = ET.fromstring(xml).findall(".//*[@event='firstQuartile']")[1].txt
-#                     self.Midpoint = ET.fromstring(xml).findall(".//*[@event='midpoint']")[1].text
-#                     self.ThirdQuartile = ET.fromstring(xml).findall(".//*[@event='thirdQuartile']")[1].text
-#                     self.Complete = ET.fromstring(xml).findall(".//*[@event='complete']")[1].text
-#                     self.ClickTracking = ET.fromstring(xml).findall(".//ClickTracking")[0].text
-#                     self.random_callback(probability=0.6)
-#                 else:
-#                     resp.failure("is_404: True. NoAdsReason: " + json_data["data"]["no_ad_reason"])
-
-
-#     def random_callback(self, probability=0.6):
-#         if(decision(probability)):
-#             self.client.get(url=self.Impression)
-#             if(decision(probability)):
-#                 self.client.get(url=self.Skip)
-#                 if(decision(probability)):
-#                     self.client.get(url=self.Progress)
-#                     if(decision(probability)):
-#                         self.client.get(url=self.FirstQuartile)
-#                         if(decision(probability)):
-#                             self.client.get(url=self.Midpoint)
-#                             if(decision(probability)):
-#                                 self.client.get(url=self.ThirdQuartile)
-#                                 if(decision(probability)):
-#                                     self.client.get(url=self.Complete)
-#                                     if(decision(probability)):
-#                                         self.client.get(url=self.ClickTracking)
+class GetOneAndCallback(HttpUser):
+    host = "https://loadtest.dev.ganjing.world/v1/cdkapi"
+    wait_time = between(5, 10)
+    @task
+    def get_onev2(self):
+        with self.client.get(url="/getonev2", params={"cid":random_text(), "cnt_id":random_text(), "req_id":random_text(), "lang":random_lang()}, name="Single_GetOne",catch_response=True) as resp:
+            code = resp.status_code
+            if code == 200:
+                json_data = json.loads(resp.text)
+                if json_data["data"]["is_404"] == False:
+                    xml = base64.b64decode(json_data["data"]["xml"])
+                    self.Impression = ET.fromstring(xml).findall(".//Impression")[1].text
+                    self.Skip = ET.fromstring(xml).findall(".//*[@event='skip']")[1].text
+                    self.Progress = ET.fromstring(xml).findall(".//*[@event='progress']")[1].text
+                    self.FirstQuartile = ET.fromstring(xml).findall(".//*[@event='firstQuartile']")[1].text
+                    self.Midpoint = ET.fromstring(xml).findall(".//*[@event='midpoint']")[1].text
+                    self.ThirdQuartile = ET.fromstring(xml).findall(".//*[@event='thirdQuartile']")[1].text
+                    self.Complete = ET.fromstring(xml).findall(".//*[@event='complete']")[1].text
+                    self.ClickTracking = ET.fromstring(xml).findall(".//ClickTracking")[0].text
+                    self.client.get(url=self.Impression, name="Impression")
+                    self.client.get(url=self.Skip, name="Skip")
+                    self.client.get(url=self.Progress, name="Progress")
+                    self.client.get(url=self.FirstQuartile, name="FirstQuartile")
+                    self.client.get(url=self.Midpoint, name="Midpoint")
+                    self.client.get(url=self.ThirdQuartile, name="ThirdQuartile")
+                    self.client.get(url=self.Complete, name="ThirdQuartile")
+                    self.client.get(url=self.ClickTracking, name="ClickTracking")
+                else:
+                    resp.failure("is_404: True. NoAdsReason: " + json_data["data"]["no_ad_reason"])
  
 
 
